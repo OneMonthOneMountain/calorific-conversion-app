15727f691f78251d1eb17cb9712dccb889755411
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-20 17:50:54 +0100

    aws creds

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
+provider "aws" {
+  region              = "eu-central-1"
+  profile             = "261496907632"
+  allowed_account_ids = ["261496907632"]
+}
+
+terraform {
+  required_version = "~> 0.11.14"
+
+  backend "s3" {
+    region  = "eu-central-1"
+    bucket  = "omom-terraform"
+    key     = "calorific-conversion.tfstate"
+    profile = "261496907632"
+  }
+}
+
 resource "aws_s3_bucket_object" "calorific_conversion_page" {
   bucket = "omom-website"
   key    = "calorific-conversion"
