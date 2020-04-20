93a6dce1a1c16ffa4be390f5542f2b5982afe070
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-20 18:02:15 +0100

    content type

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,8 @@ terraform {
 
 resource "aws_s3_bucket_object" "calorific_conversion_page" {
   bucket = "omom-website"
-  key    = "calorific-conversion.html"
+  key    = "calorific-conversion"
   source = "../src/index.html"
+
+  content_type = "text/html"
 }
