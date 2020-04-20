f7043c1dc06457f3812d1a0a33b28b7dd79878ac
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-20 18:35:27 +0100

    Made pages

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,30 @@ terraform {
 
 resource "aws_s3_bucket_object" "calorific_conversion_page" {
   bucket = "omom-website"
-  key    = "calorific-conversion"
-  source = "../src/index.html"
+  key    = "calorific-conversion.html"
+  source = "../website/calorific-conversion.html"
 
-  etag = "${filemd5("../src/index.html")}"
+  etag = "${filemd5("../website/calorific-conversion.html")}"
+
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "leaderboard_page" {
+  bucket = "omom-website"
+  key    = "leaderboard.html"
+  source = "../website/leaderboard.html"
+
+  etag = "${filemd5("../website/leaderboard.html")}"
+
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "login_page" {
+  bucket = "omom-website"
+  key    = "login.html"
+  source = "../website/login.html"
+
+  etag = "${filemd5("../website/login.html")}"
 
   content_type = "text/html"
 }
