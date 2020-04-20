abb11a0e07470570dfd061620832bc12796846c6
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-20 17:41:45 +0100

    terraform init (should fail)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "calorific_conversion_page" {
-   bucket = "omom-website"
+  bucket = "omom-website"
   key    = "calorific-conversion"
   source = "../src/index.html"
 }
