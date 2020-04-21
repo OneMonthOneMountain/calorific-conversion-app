882cc4e118f46f59736abca639bda2ff73dfaf1b
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-21 22:53:45 +0100

    lambda wiring

--- lambda/index.js
+++ lambda/index.js
@@ -1,16 +1,20 @@
 const S3Client = require('aws-sdk/clients/s3')
+const { DocumentClient } = require('aws-sdk/clients/dynamodb')
 
 const s3 = new S3Client()
+const dynamo = new DocumentClient()
 
 const handler = async () => {
-    const scores = [
-        {
-            name: "Tom",
-            distance: 1,
-            ascent: 25,
-            calories: 100,
-        }
-    ]
+    const rawScores = await dynamo.scan({
+        TableName: 'scores'
+    }).promise()
+    
+    const scores = rawScores.Items.map((score) => ({
+        name: score.UserId,
+        distance: score.Distance,
+        ascent: score.Ascent,
+        calories: score.Calories
+    }))
     
     await s3.putObject({
         Bucket: 'omom-website',
