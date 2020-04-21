aa70bfadfe78a88e3c2961a77647d082dea045d0
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-21 22:34:12 +0100

    enable stream

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,6 +134,9 @@ resource "aws_dynamodb_table" "table" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "UserId"
 
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
   attribute {
     name = "UserId"
     type = "S"
