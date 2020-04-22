a769e54636a47a23bd45e05c8ef3f0dfda39272d
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-22 23:34:32 +0100

    CORS

--- lambda/get-user/index.js
+++ lambda/get-user/index.js
@@ -2,26 +2,42 @@ const { DocumentClient } = require('aws-sdk/clients/dynamodb')
 
 const dynamo = new DocumentClient()
 
-const handler = async (event) => {
-	const id = event.queryStringParameters.id
+const headers = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': '*',
+	'Access-Control-Allow-Methods': '*',
+	'Content-Type': 'application/json'
+}
 
-	if (!id) {
-		throw Error('Missing id')
-	}
-	
-	const { Item } = await dynamo.get({
-	    TableName: 'scores',
-	    Key: { "UserId": id }
-	}).promise()
-	
-	return {
-		statusCode: 200,
-		body: JSON.stringify({
-			id: Item.UserId,
-			name: Item.Name,
-			activities: Item.Activities,
-			calorieUnit: Item.CalorieUnit
-		})
+const handler = async (event) => {
+	try {
+		const id = event.queryStringParameters.id
+
+		if (!id) {
+			throw Error('Missing id')
+		}
+		
+		const { Item } = await dynamo.get({
+			TableName: 'scores',
+			Key: { "UserId": id }
+		}).promise()
+		
+		return {
+			statusCode: 200,
+			headers,
+			body: JSON.stringify({
+				id: Item.UserId,
+				name: Item.Name,
+				activities: Item.Activities,
+				calorieUnit: Item.CalorieUnit
+			})
+		}
+	} catch (error) {
+		return {
+			statusCode: 500,
+			headers,
+			body: error.message
+		}
 	}
 }
 
--- lambda/register-user/index.js
+++ lambda/register-user/index.js
@@ -2,27 +2,43 @@ const { DocumentClient } = require('aws-sdk/clients/dynamodb')
 
 const dynamo = new DocumentClient()
 
-const handler = async (event) => {
-	const user = JSON.parse(event.body)
+const headers = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': '*',
+	'Access-Control-Allow-Methods': '*',
+	'Content-Type': 'application/json'
+}
 
-	if (!user.id || !user.name || !user.calorieUnit) {
-		console.log('User missing a required value: ', JSON.stringify(user))
-		throw Error('Missing required value')
-	}
-	
-	await dynamo.put({
-	    TableName: 'scores',
-	    Item: {
-	        "UserId": user.id,
-	        "Name": user.name,
-	        "CalorieUnit": user.calorieUnit,
-	        "Activities": user.activities
-	    }
-	}).promise()
-	
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ status: 'SUCCESS' })
+const handler = async (event) => {
+	try {
+		const user = JSON.parse(event.body)
+
+		if (!user.id || !user.name || !user.calorieUnit) {
+			console.log('User missing a required value: ', JSON.stringify(user))
+			throw Error('Missing required value')
+		}
+		
+		await dynamo.put({
+			TableName: 'scores',
+			Item: {
+				"UserId": user.id,
+				"Name": user.name,
+				"CalorieUnit": user.calorieUnit,
+				"Activities": user.activities
+			}
+		}).promise()
+		
+		return {
+			statusCode: 200,
+			headers,
+			body: JSON.stringify({ status: 'SUCCESS' })
+		}
+	} catch (error) {
+		return {
+			statusCode: 500,
+			headers,
+			body: error.message
+		}
 	}
 }
 
