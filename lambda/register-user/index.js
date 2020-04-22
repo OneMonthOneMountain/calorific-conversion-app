5ede084cade76b88f746da739b02db952d26719b
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-22 01:17:40 +0100

    Add user

--- lambda/register-user/index.js
+++ lambda/register-user/index.js
@@ -1,5 +1,16 @@
-const handler = () => {
-	console.log('hello')
+const { DocumentClient } = require('aws-sdk/clients/dynamodb')
+
+const dynamo = new DocumentClient()
+
+const handler = async ({ name, calorieUnit }) => {
+	await dynamo.put({
+	    TableName: 'scores',
+	    Item: {
+	        "UserId": name,
+	        "CalorieUnit": calorieUnit,
+	        "Activities": {}
+	    }
+	}).promise()
 }
 
 module.exports = { handler }
