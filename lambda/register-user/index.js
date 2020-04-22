3cae3ec176ae4de57d43f9ff53d69b680e44576b
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-22 01:32:03 +0100

    api

--- lambda/register-user/index.js
+++ lambda/register-user/index.js
@@ -2,15 +2,21 @@ const { DocumentClient } = require('aws-sdk/clients/dynamodb')
 
 const dynamo = new DocumentClient()
 
-const handler = async ({ name, calorieUnit }) => {
+const handler = async (event) => {
+	const user = JSON.parse(event.body)
+	
 	await dynamo.put({
 	    TableName: 'scores',
 	    Item: {
-	        "UserId": name,
-	        "CalorieUnit": calorieUnit,
+	        "UserId": user.name,
+	        "CalorieUnit": user.calorieUnit,
 	        "Activities": {}
 	    }
 	}).promise()
+	
+	return {
+		statusCode: 200
+	}
 }
 
 module.exports = { handler }
