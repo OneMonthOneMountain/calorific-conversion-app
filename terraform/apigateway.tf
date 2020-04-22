13f0e76cbde4533b7a49ab03cfa1f93958f89878
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-22 01:22:57 +0100

    endpoint

---
+++ terraform/apigateway.tf
@@ -0,0 +1,56 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "scores"
+  description = "API for updating peoples scores"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+  tags = "${local.tags}"
+}
+
+resource "aws_api_gateway_resource" "register_user" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "register-user"
+}
+
+resource "aws_api_gateway_method" "register_user" {
+  rest_api_id      = "${aws_api_gateway_rest_api.api.id}"
+  resource_id      = "${aws_api_gateway_resource.register_user.id}"
+  http_method      = "POST"
+  authorization    = "NONE"
+  api_key_required = "false"
+}
+
+resource "aws_api_gateway_integration" "register_user" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.register_user.id}"
+  http_method = "${aws_api_gateway_method.register_user.http_method}"
+  type        = "AWS_PROXY"
+
+  uri                     = "${aws_lambda_function.register_user_lamba.invoke_arn}"
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_deployment" "register_user" {
+  depends_on = [
+    "aws_api_gateway_method.register_user",
+    "aws_api_gateway_integration.register_user",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "default"
+}
+
+resource "aws_lambda_permission" "register_user" {
+  statement_id  = "AllowAPIGateway-${uuid()}"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.register_user_lamba.arn}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_deployment.register_user.execution_arn}/*"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
