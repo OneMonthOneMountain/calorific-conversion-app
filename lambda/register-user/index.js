b7bdc8d89d415de2ea05bb6bf7bb3d372a0f4aee
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-04-22 22:03:15 +0100

    id and name

--- lambda/register-user/index.js
+++ lambda/register-user/index.js
@@ -4,11 +4,17 @@ const dynamo = new DocumentClient()
 
 const handler = async (event) => {
 	const user = JSON.parse(event.body)
+
+	if (!user.id || !user.name || !user.calorieUnit) {
+		console.log('User missing a required value: ', JSON.stringify(user))
+		throw Error('Missing required value')
+	}
 	
 	await dynamo.put({
 	    TableName: 'scores',
 	    Item: {
-	        "UserId": user.name,
+	        "UserId": user.id,
+	        "Name": user.name,
 	        "CalorieUnit": user.calorieUnit,
 	        "Activities": {}
 	    }
