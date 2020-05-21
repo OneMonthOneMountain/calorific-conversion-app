06df76083203004d9c265ac10ca7865d89e28e57
tomwlloyd <tom.w.lloyd@googlemail.com>
2020-05-21 11:19:53 +0100

    New Bucket

--- lambda/calculate-scores/index.js
+++ lambda/calculate-scores/index.js
@@ -50,7 +50,7 @@ const handler = async () => {
         totals.mountain = mountains.find((mountain) => mountain.height < totals.ascent)
         
         await s3.putObject({
-            Bucket: 'omom-website',
+            Bucket: 'omom-virtual-hiker',
             Key: 'scores.json',
             Body: JSON.stringify({ scores, totals }),
             ContentType: 'application/json',
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,36 @@
+resource "aws_s3_bucket" "virtual_hiker" {
+  bucket = "omom-virtual-hiker"
+  acl    = "public-read"
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadForGetBucketObjects",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::omom-virtual-hiker/*"
+        }
+    ]
+}
+POLICY
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
+  website {
+    index_document = "calorific-conversion.html"
+    error_document = "calorific-conversion.html"
+  }
+}
 
 resource "aws_s3_bucket_object" "calorific_conversion_page" {
-  bucket = "omom-website"
+  bucket = "${aws_s3_bucket.virtual_hiker.id}"
   key    = "calorific-conversion.html"
   source = "../website/calorific-conversion.html"
 
@@ -10,7 +40,7 @@ resource "aws_s3_bucket_object" "calorific_conversion_page" {
 }
 
 resource "aws_s3_bucket_object" "leaderboard_page" {
-  bucket = "omom-website"
+  bucket = "${aws_s3_bucket.virtual_hiker.id}"
   key    = "leaderboard.html"
   source = "../website/leaderboard.html"
 
@@ -20,7 +50,7 @@ resource "aws_s3_bucket_object" "leaderboard_page" {
 }
 
 resource "aws_s3_bucket_object" "user_page" {
-  bucket = "omom-website"
+  bucket = "${aws_s3_bucket.virtual_hiker.id}"
   key    = "user.html"
   source = "../website/user.html"
 
